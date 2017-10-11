ddd5df7d5a3e00629b1f811558a13f1daaca019d
sinplo <32623677+sinplo@users.noreply.github.com>
2017-10-11 17:52:45 +0300

    palindrome

--- src/problems_1_to_25/Problem_4_Palindrome.java
+++ src/problems_1_to_25/Problem_4_Palindrome.java
@@ -1,14 +1,48 @@
 package problems_1_to_25;
 
 public class Problem_4_Palindrome {
-//	A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-//
-//			Find the largest palindrome made from the product of two 3-digit numbers.
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// A palindromic number reads the same both ways. The largest palindrome
+	// made from the product of two 2-digit numbers is 9009 = 91 × 99.
+	//
+	// Find the largest palindrome made from the product of two 3-digit numbers.
 
+	public static int largestPalindromeOf3() {
+		int palindrome = 0;
+
+		for (int a = 999; a >= 100; a--) {
+			for (int b = 999; b >= 100; b--) {
+				int number = a * b;
+				if (isPalindrome(number) && number > palindrome) {
+					palindrome = number;
+				}
+			}
+		}
+
+		return palindrome;
+	}
+
+	public static boolean isPalindrome(int number) {
+		boolean isPalindrome = false;
+		String tempString = Integer.toString(number);
+		int indexA = 0;
+		int indexB = tempString.length() - 1;
+
+		while (indexA < 3 && indexB > 2) {
+			if (tempString.charAt(indexA) == tempString.charAt(indexB)) {
+				isPalindrome = true;
+				indexA++;
+				indexB--;
+			} else {
+				isPalindrome = false;
+				break;
+			}
+		}
+		return isPalindrome;
+	}
+
+	public static void main(String[] args) {
+		boolean palindrome = isPalindrome(100091);
+		System.out.println(largestPalindromeOf3());
 	}
 
 }
